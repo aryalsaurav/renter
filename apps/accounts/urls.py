62720f037448ba3b3fabf308562340b0e14d62f6
Saurav Aryal <aryalsaurav01@gmail.com>
2026-07-14 11:46:43 +0545

    chore: burn cpu to check hpa

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("logout/", views.RenterLogoutView.as_view(), name="logout"),
     path("signup/", views.signup_view, name="signup"),
     path("profile/", views.profile_view, name="profile"),
+    path("burn-cpu/", views.burn_cpu, name="burn-cpu"),
 ]
